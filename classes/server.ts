2cfc99e37fd32ed2beaba659622b05bd2df5eb7c
miguel <miguelcr1982@gmail.com>
2021-06-14 20:19:26 -0600

    Patrón sigleton

--- classes/server.ts
+++ classes/server.ts
@@ -4,6 +4,7 @@ import socketIO from 'socket.io';
 import http from 'http';
 
 export default class Server {
+  private static _instance: Server;
   public app: express.Application;
   public port: number;
 
@@ -15,23 +16,26 @@ export default class Server {
     this.port = SERVER_PORT;
 
     this.httpServer = new http.Server(this.app);
-    this.io = require("socket.io")(this.httpServer, {
+    this.io = require('socket.io')(this.httpServer, {
       cors: {
         origin: true,
-        credentials: true;
-      }
-    })
+        credentials: true,
+      },
+    });
 
     this.escucharSockets();
   }
 
-  private escucharSockets(){
+  public static get instance() {
+    return this._instance || (this._instance = new this());
+  }
+
+  private escucharSockets() {
     console.log('Escuchando conexiones - socket');
 
-    this.io.on('connection', cliente=> {
+    this.io.on('connection', (cliente) => {
       console.log('Cliente conectado');
-    })
-    
+    });
   }
 
   start(callback: () => void) {
