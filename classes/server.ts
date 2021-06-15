0a02b34ae328530c4bc227e0036cbdcc556c4404
miguel <miguelcr1982@gmail.com>
2021-06-14 19:59:57 -0600

    Implementar socket

--- classes/server.ts
+++ classes/server.ts
@@ -1,16 +1,40 @@
 import express from 'express';
 import { SERVER_PORT } from '../global/environment';
+import socketIO from 'socket.io';
+import http from 'http';
 
 export default class Server {
   public app: express.Application;
   public port: number;
 
+  public io: socketIO.Server;
+  private httpServer: http.Server;
+
   constructor() {
     this.app = express();
     this.port = SERVER_PORT;
+
+    this.httpServer = new http.Server(this.app);
+    this.io = require("socket.io")(this.httpServer, {
+      cors: {
+        origin: true,
+        credentials: true;
+      }
+    })
+
+    this.escucharSockets();
+  }
+
+  private escucharSockets(){
+    console.log('Escuchando conexiones - socket');
+
+    this.io.on('connection', cliente=> {
+      console.log('Cliente conectado');
+    })
+    
   }
 
   start(callback: () => void) {
-    this.app.listen(this.port, callback);
+    this.httpServer.listen(this.port, callback);
   }
 }
