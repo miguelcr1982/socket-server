8049b13cedf7a1240feec588f626cec4a1b9deb1
miguel <miguelcr1982@gmail.com>
2021-06-14 21:04:48 -0600

    Detectar desconexión

--- classes/server.ts
+++ classes/server.ts
@@ -2,6 +2,7 @@ import express from 'express';
 import { SERVER_PORT } from '../global/environment';
 import socketIO from 'socket.io';
 import http from 'http';
+import * as socket from '../sockets/socket';
 
 export default class Server {
   private static _instance: Server;
@@ -35,6 +36,9 @@ export default class Server {
 
     this.io.on('connection', (cliente) => {
       console.log('Cliente conectado');
+
+      //desconectar.
+      socket.desconectar(cliente);
     });
   }
 
